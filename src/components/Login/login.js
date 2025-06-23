482da3041c311e2d4fc37fc1810ed2f3364e3806
Hoang Son <yani.gmaing123@gmail.com>
2025-06-23 23:43:41 +0700

    create form login

--- src/components/Login/login.js
+++ src/components/Login/login.js
@@ -2,22 +2,25 @@ import React from 'react';
 import "./Login.scss"
 const Login = (props) => {
     return (
-        <div className='login-container mt-3'>
+        <div className='login-container'>
             <div className='container'>
-                <div className='row'>
-                    <div className='content-left col-7'>
+                <div className='row px-3 px-sm-0'>
+                    <div className='content-left col-12 d-none col-sm-7 d-sm-block'>
                         <div className='brand'>
                             Well come
                         </div>
                         <div className='detail'>
-                            Hello ! my name is Son , nice to meet you '-'
+                            My name is Son , nice to meet you '-'
                         </div>
                     </div>
-                    <div className='content-right col-5 d-flex flex-column gap-3 py-3'>
+                    <div className='content-right col-sm-5 col-12 d-flex flex-column gap-3 py-3'>
+                        <div className='brand d-sm-none'>
+                            Well come
+                        </div>
                         <input type='text' className='form-control' placeholder='Email address or phone number' />
                         <input type='text' className='form-control' placeholder='Your password' />
                         <button className='btn btn-warning'>Login</button>
-                        <span className='text-center'>Forgot your password?</span>
+                        <span className='text-center'><a className='forgot-password' href='#'>Forgot your password?</a></span>
                         <hr />
                         <div className='text-center'>
                             <button className='btn btn-primary'>Create new account</button>
